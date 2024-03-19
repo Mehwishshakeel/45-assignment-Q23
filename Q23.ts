004aa3c11f2a6ec3a39459e9b01f2e6ec75a43e2
Mehwish Shakeel <160312242+Mehwishshakeel@users.noreply.github.com>
2024-03-20 00:27:29 +0500

    Add files via upload

---
+++ Q23.ts
@@ -0,0 +1,34 @@
+//Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test.
+// making a variable;
+let two = 2;
+let ten = 10;
+// test one;
+console.log("\n test 1:two is eqaul to 2?")
+console.log(two ==2);
+// Test 2
+console.log("\n Test 2:ten is equal to 10");
+console.log(ten==10);
+//Test 3
+console.log("\n Test 3: two is not equal to 10");
+console.log(two!=10);
+//Test 4
+console.log("\n Test 4: test is greater then two");
+console.log(10>2);
+//Test 5
+console.log("\n Test 5: two is smalller then 10");
+console.log(two<10);
+// Test 6
+console.log("\n Test 6: 2 is greater then ten");
+console.log(2 > 10);
+//Test 7
+console.log("\n Test 7: ten is equal to two");
+console.log(ten== 2 );
+// Test 8
+console.log("\n Test 8: ten is less then two");
+console.log(10<2);
+//Test 9
+console.log("\n Test 9: two is not equal to two");
+console.log(two!=2);
+//Test 10
+console.log("\n Test 10: 10 is not equal to 10");
+console.log(10!=10);
